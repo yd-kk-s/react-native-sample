3f1b0f1eb0230ea23a4ba9e74cd870d503a51e23
Kanako Yamada <40951963+yd-kk-s@users.noreply.github.com>
2021-02-16 22:12:27 +0900

    detail screen

--- src/components/screens/DetailScreen.tsx
+++ src/components/screens/DetailScreen.tsx
@@ -1,10 +1,79 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
+import { ENVs } from '../../consts/envs'
 
-function DetailScreen() {
+import { Review } from '../../domains/Review'
+
+import Geocoder from 'react-native-geocoding';
+import MapView from 'react-native-maps';
+
+const MAP_ZOOM_RATE = 15.0;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+interface Props {
+  navigation: {},
+  route: {
+    key: string,
+    name: string,
+    params: {
+      selectedReview: Review
+    }
+  }
+}
+
+function DetailScreen(props: Props) {
+  // console.log(props)
+  const review = props.route.params.selectedReview
+  const [isMapLoading, setIsMapLoading] = useState(true);
+  const [initialRegion, setInitialRegion] = useState({ latitude: 35.7090,  //緯度
+                                                       longitude: 139.7320, //経度
+                                                       latitudeDelta: MAP_ZOOM_RATE,
+                                                       longitudeDelta: MAP_ZOOM_RATE * 2.25
+                                                     })
+
+  useEffect(() => {
+    (async() => {
+      try {
+        Geocoder.init(ENVs.geocodingAPI);
+        const result = await Geocoder.from(review.country);
+        const location = result.results[0].geometry.location;
+
+        setInitialRegion({
+          latitude: location.lat,
+          longitude: location.lng,
+          latitudeDelta: MAP_ZOOM_RATE,
+          longitudeDelta: MAP_ZOOM_RATE * 2.25
+        })
+        setIsMapLoading(false)
+
+      } catch(err) {
+        console.log(err)
+      }  
+    })();
+  },[])
+
+  if (isMapLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    )
+  }
+
   return(
-    <View style={styles.container}>
-      <Text>DetailScreen!</Text>
+    <View style={{ flex: 1 }}>
+      <ScrollView>
+        <View style={{ alignItems: 'center', padding: 20 }}>
+          <Text style={{ fontSize: 30, padding: 5 }}>{review.country}</Text>
+          <Text style={{ padding: 5 }}>{review.dateFrom} ~ {review.dateTo}</Text>
+        </View>
+
+        <MapView
+          style={{ height: SCREEN_WIDTH }}
+          scrollEnabled={false}
+          initialRegion={initialRegion}
+        />
+      </ScrollView>
     </View>
   )
 }
